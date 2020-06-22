57e0da08ae8df16e0b981ee7f832eacc74f28618
KodexDigital <33692534+KodexDigital@users.noreply.github.com>
2020-06-22 16:51:43 +0100

    Added viemodel to the application

--- MailSenderApp/Controllers/HomeController.cs
+++ MailSenderApp/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MailSenderApp.Models;
+using MailSenderApp.ViewModels;
+using System.Net.Mail;
+using System.Net;
 
 namespace MailSenderApp.Controllers
 {
@@ -16,6 +19,42 @@ namespace MailSenderApp.Controllers
 		public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
+		}
+
+		public IActionResult SendMail()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult SendMail(MailSenderViewModel model)
+		{
+			if(ModelState.IsValid)
+			{
+				MailMessage msg = new MailMessage
+				{
+					From = new MailAddress("digitalkenth@gmail.com"),
+				};
+				msg.To.Add(model.RecipientEmail);
+				msg.Subject = model.Title;
+				msg.Body = model.Body;
+
+				SmtpClient client = new SmtpClient
+				{
+					Host = "smtp.gmail.com"
+				};
+				NetworkCredential credential = new NetworkCredential
+				{
+					UserName = "aptechweb2019@gmail.com",
+					Password = "aptech2019@"
+				};
+				client.Credentials = credential;
+				client.EnableSsl = true;
+				client.Port = 587;
+				client.Send(msg);
+
+				ViewBag.Success = $"Email has been sent successfully to {model.RecipientEmail}";
+			}
+			return View(model);
 		}
 
 		public IActionResult Index()
---
+++ MailSenderApp/ViewModels/MailSenderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailSenderApp.ViewModels
+{
+	public class MailSenderViewModel
+	{
+		[Display(Name ="Subject")]
+		public string Title { get; set; }
+		[Display(Name = "To")]
+		public string RecipientEmail { get; set; }
+		public string Body { get; set; }
+		public string Attachments { get; set; }
+	}
+}
